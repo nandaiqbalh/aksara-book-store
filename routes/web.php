8ed8f21365ccaa034ffe60052c10c650257bd9e6
nandaiqbalh <nandaiqbalhanafii@gmail.com>
2022-05-27 16:38:10 +0700

    [Middleware] Fixing protect with middleware

--- routes/web.php
+++ routes/web.php
@@ -31,54 +31,57 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function ()
 // kata admin setelah sacntum adalah nama guard
 Route::middleware(['auth:sanctum,admin', 'verified'])->get('/admin/dashboard', function () {
     return view('admin.index');
-})->name('admin.dashboard');
+})->name('admin.dashboard')->middleware('auth:admin');
 
 
-// ADMIN ALL ROUTES
-Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+Route::middleware(['auth:admin'])->group(function(){
+    // ADMIN ALL ROUTES
+    Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
 
 // profile
-Route::get('/admin/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
+    Route::get('/admin/profile', [AdminProfileController::class, 'adminProfile'])->name('admin.profile');
 
-Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
+    Route::get('/admin/profile/edit', [AdminProfileController::class, 'adminProfileEdit'])->name('admin.profile.edit');
 
-Route::post('/admin/profile/store', [AdminProfileController::class, 'adminProfileStore'])->name('admin.profile.store');
+    Route::post('/admin/profile/store', [AdminProfileController::class, 'adminProfileStore'])->name('admin.profile.store');
 
 // password
-Route::get('/admin/change/password', [AdminProfileController::class, 'adminChangePassword'])->name('admin.change.password');
+    Route::get('/admin/change/password', [AdminProfileController::class, 'adminChangePassword'])->name('admin.change.password');
 
-Route::post('/admin/update/password', [AdminProfileController::class, 'adminUpdatePassword'])->name('admin.update.password');
+    Route::post('/admin/update/password', [AdminProfileController::class, 'adminUpdatePassword'])->name('admin.update.password');
 
 
-// ADMIN CATEGORY ALL ROUTES
-Route::prefix('category')->group(function () {
-    Route::get('/view', [CategoryController::class, 'categoryView'])->name('all.category');
+// ADMIN CATEGORY ALL  ROUTES
+    Route::prefix('category')->group(function () {
+        Route::get('/view', [CategoryController::class, 'categoryView'])->name('all.category');
 
-    Route::post('/store', [CategoryController::class, 'categoryStore'])->name('category.store');
+        Route::post('/store', [CategoryController::class, 'categoryStore'])->name('category.store');
 
-    Route::get('/edit/{id}', [CategoryController::class, 'categoryEdit'])->name('category.edit');
+        Route::get('/edit/{id}', [CategoryController::class, 'categoryEdit'])->name('category.edit');
 
-    Route::post('/update', [CategoryController::class, 'categoryUpdate'])->name('category.update');
+        Route::post('/update', [CategoryController::class, 'categoryUpdate'])->name('category.update');
 });
 
-// ADMIN BOOK ALL ROUTES
-Route::prefix('book')->group(function () {
-    Route::get('/add', [BookController::class, 'bookAdd'])->name('book.add');
+// ADMIN BOOK ALL  ROUTES
+    Route::prefix('book')->group(function () {
+        Route::get('/add', [BookController::class, 'bookAdd'])->name('book.add');
 
-    Route::post('/store', [BookController::class, 'bookStore'])->name('book.store');
+        Route::post('/store', [BookController::class, 'bookStore'])->name('book.store');
 
-    Route::get('/manage', [BookController::class, 'bookManage'])->name('book.manage');
+        Route::get('/manage', [BookController::class, 'bookManage'])->name('book.manage');
 
-    Route::get('/edit/{id}', [BookController::class, 'bookEdit'])->name('book.edit');
+        Route::get('/edit/{id}', [BookController::class, 'bookEdit'])->name('book.edit');
 
-    Route::post('/update', [BookController::class, 'bookUpdate'])->name('book.update');
+        Route::post('/update', [BookController::class, 'bookUpdate'])->name('book.update');
 
-    Route::get('/inactive/{id}', [BookController::class, 'bookInactive'])->name('book.inactive');
-    Route::get('/active/{id}', [BookController::class, 'bookActive'])->name('book.active');
+        Route::get('/inactive/{id}', [BookController::class, 'bookInactive'])->name('book.inactive');
+        Route::get('/active/{id}', [BookController::class, 'bookActive'])->name('book.active');
 
-    Route::get('/delete/{id}', [BookController::class, 'bookDelete'])->name('book.delete');
+        Route::get('/delete/{id}', [BookController::class, 'bookDelete'])->name('book.delete');
 
+    });
 });
+
 
 
 // kata web setelah sacntum adalah nama guard
